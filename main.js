8aa7ee8e7d9ecc86cdee78da37aa45edc5be2fb9
Alessandro Camargo <acamargodias@terra.com.br>
2022-09-30 17:20:35 -0300

    ajustes

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ function calcShipping() {
     const valor = Number(document.getElementById('valor').value);
     const peso = Number(document.getElementById('peso').value);
 
-    var despacho = Number.parseFloat("10.15")
-    var pedagio = Number.parseFloat(pedag(peso))
-    var frete = Number.parseFloat(classification(peso))
-    var fretevalor = Number(valor * 0.23) / 100
-    var gris = parseFloat(valor * 0.17) / 100
-    var imposto = parseFloat(despacho + pedagio + frete + fretevalor + gris) * 13.65 / 100
+    let despacho = Number.parseFloat("10.15")
+    let pedagio = Number.parseFloat(pedag(peso))
+    let frete = Number.parseFloat(classification(peso))
+    let fretevalor = Number(valor * 0.23) / 100
+    let gris = parseFloat(valor * 0.17) / 100
+    let imposto = parseFloat(despacho + pedagio + frete + fretevalor + gris) * 13.65 / 100
 
     let isValid = validateCalc(valor, peso);
 
     if (isValid == true) {
         const result = Number(frete + despacho + pedagio + fretevalor + gris + imposto).toFixed(2);
-        document.getElementById('result').innerHTML = `O valor do frete é de \u{1F449} R$ ${result}`
+        document.getElementById('results').innerHTML = `O valor do frete é de \u{1F449} R$ ${result}`
     }
 }
 
@@ -67,7 +67,7 @@ function classification(peso) {
 function clearValues() {
     valor.value = "";
     peso.value = "";
-    result.innerHTML = "";
+    results.innerHTML = "";
 }
 
 
@@ -128,6 +128,113 @@ document.getElementById('clockNow').innerHTML = `${hours}:${minutes}:${seconds}`
 }
 
 setInterval(hourNow, 1000);
+
+const input = document.getElementById("input");
+const result = document.getElementById("result");
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+const moon = document.getElementById("moon");
+const light = document.getElementById("light");
+const copy = document.getElementById("copyToClipboard");
+moon.style.display = "none";
+light.style.display = "block";
+
+const allowedKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  " ",
+];
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+  }
+  if (ev.key === "Enter") {
+    calculate();
+  }
+  if (ev.key === "Backspace" || ev.key === "Delete") {
+    input.value = input.value.slice(0, -1);
+  }
+  if (ev.key === "Escape") {
+    input.value = "";
+    input.focus();
+  }
+});
+
+document.getElementById("delete").addEventListener("click", function () {
+  input.value = input.value.slice(0, -1);
+});
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = "";
+  input.focus();
+  if ((result.value = "ERRO")) {
+    result.classList.remove("error");
+    result.value = "";
+  }
+});
+
+document.getElementById("equal").addEventListener("click", calculate);
+
+function calculate() {
+  result.value = "ERRO";
+  result.classList.add("error");
+  const equal = eval(input.value);
+  result.value = equal;
+  result.classList.remove("error");
+}
+
+document.querySelectorAll(".charKey").forEach(function (char) {
+  char.addEventListener("click", function () {
+    const value = char.dataset.value;
+    input.value += value;
+  });
+});
+
+document.getElementById("theme").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#fff");
+    root.style.setProperty("--font-color", "#000");
+    root.style.setProperty("--primary-color", "#855b0e");
+    moon.style.display = "block";
+    light.style.display = "none";
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#333");
+    root.style.setProperty("--font-color", "#fff");
+    root.style.setProperty("--primary-color", "#fbab18");
+    moon.style.display = "none";
+    light.style.display = "block";
+    main.dataset.theme = "dark";
+  }
+});
+
+copy.addEventListener("click", function (ev) {
+  if (copy.innerText === "Cópia") {
+    copy.innerText = "Copiado";
+    copy.classList.add("success");
+    navigator.clipboard.writeText(result.value);
+  } else {
+    copy.innerText = "Cópia";
+    copy.classList.remove("success");
+  }
+});
 
 
 
